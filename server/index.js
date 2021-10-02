e67d7a7f8dfe4cf92e19d869e307730a9e2713db
john <johnny>
2021-10-02 17:01:59 -0500

    start server file

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const passport = require("passport")
+
+const morgan = require("morgan")
+const compression = require("compression")
+
+const app = express()
+
+
+const PORT = process.env.PORT || 8080
+
+
+const createApp = () => {
+    // logging middleware
+    app.use(morgan('dev'))
+
+  
+
+    // error handling endware
+    app.use((err, req, res, next) => {
+      console.error(err)
+      console.error(err.stack)
+      res.status(err.status || 500).send(err.message || 'Internal server error.')
+    })
+  }
+  
+
+
+
+  const startListening = () => {
+    // start listening (and create a 'server' object representing our server)
+    const server = app.listen(PORT, () =>
+      console.log(`Listening on port: ${PORT}`)
+    )
+  
+  }
+
+
+  async function bootApp() {
+    await createApp()
+    await startListening()
+  }
+
+
+if (require.main === module) {
+    bootApp()
+} else {
+    createApp()
+}
