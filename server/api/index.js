93b085d70f90401ce9f63b4db595130c513c6e53
john <johnny>
2021-10-03 13:51:32 -0500

    start of student route

---
+++ server/api/index.js
@@ -0,0 +1,14 @@
+
+const router = require('express').Router()
+module.exports = router
+
+router.use("/students", require("./students"))
+
+
+
+
+router.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+  })
---
+++ server/api/students.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { Student } = require("../db/models");
+
+
+
+
+module.exports = router;
