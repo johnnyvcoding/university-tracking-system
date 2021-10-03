b00e96ec9bca4ec4944831c6e597bc8c86d6ca43
john <johnny>
2021-10-03 14:00:58 -0500

    query search for students

--- server/api/students.js
+++ server/api/students.js
@@ -3,14 +3,27 @@ const { Student } = require("../db/models");
 
 router.get("/", async (req, res, next) => {
   try {
+    let students = await Student.findAll({});
+
+    return res.json(students);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
     let { firstName, lastName } = req.query;
 
-    let student = await Student.findOne({
+    let students = await Student.findAll({
       where: {
         firstName: firstName,
         lastName: lastName,
       },
     });
+
+    return res.json(students);
   } catch (error) {
     console.log(error);
     next(error);
