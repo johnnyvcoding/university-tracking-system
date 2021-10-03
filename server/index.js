20be2c97b1fc4cc161524a157b3f7d61582eef95
john <johnny>
2021-10-03 13:42:58 -0500

    session storage and serialzing for users

--- server/index.js
+++ server/index.js
@@ -15,9 +15,33 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sessionStore = new SequelizeStore({ db });
 const PORT = process.env.PORT || 8080;
 
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.User.findByPk(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 const createApp = () => {
   // logging middleware
   app.use(morgan("dev"));
+
+  // session middleware with passport
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || "<scecret_key>",
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   // error handling endware
   app.use((err, req, res, next) => {
@@ -34,7 +58,11 @@ const startListening = () => {
   );
 };
 
+const syncDb = () => db.sync();
+
 async function bootApp() {
+  await sessionStore.sync()
+  await syncDb()
   await createApp();
   await startListening();
 }
