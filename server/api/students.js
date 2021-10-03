b2d31f53ced15ee27eec5d2bb844df00a8bc1e70
john <johnny>
2021-10-03 13:55:13 -0500

    get student via query params

--- server/api/students.js
+++ server/api/students.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
 const { Student } = require("../db/models");
 
+router.get("/", async (req, res, next) => {
+  try {
+    let { firstName, lastName } = req.query;
 
-
+    let student = await Student.findOne({
+      where: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 module.exports = router;
