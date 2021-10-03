e15eca849c9f2f19e09bbf4fda5a6460d3bb8cd0
john <johnny>
2021-10-03 13:35:09 -0500

    session sstorage for users using our db

--- server/index.js
+++ server/index.js
@@ -1,49 +1,46 @@
-const express = require("express")
-const passport = require("passport")
+const express = require("express");
+const passport = require("passport");
 
-const morgan = require("morgan")
-const compression = require("compression")
+const morgan = require("morgan");
+const compression = require("compression");
 
-const app = express()
+const app = express();
 
+const db = require("./db/index");
 
-const PORT = process.env.PORT || 8080
+const session = require("express-session");
 
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sessionStore = new SequelizeStore({ db });
+const PORT = process.env.PORT || 8080;
 
 const createApp = () => {
-    // logging middleware
-    app.use(morgan('dev'))
+  // logging middleware
+  app.use(morgan("dev"));
 
-  
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
+};
 
-    // error handling endware
-    app.use((err, req, res, next) => {
-      console.error(err)
-      console.error(err.stack)
-      res.status(err.status || 500).send(err.message || 'Internal server error.')
-    })
-  }
-  
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  const server = app.listen(PORT, () =>
+    console.log(`Listening on port: ${PORT}`)
+  );
+};
 
-
-
-  const startListening = () => {
-    // start listening (and create a 'server' object representing our server)
-    const server = app.listen(PORT, () =>
-      console.log(`Listening on port: ${PORT}`)
-    )
-  
-  }
-
-
-  async function bootApp() {
-    await createApp()
-    await startListening()
-  }
-
+async function bootApp() {
+  await createApp();
+  await startListening();
+}
 
 if (require.main === module) {
-    bootApp()
+  bootApp();
 } else {
-    createApp()
+  createApp();
 }
