7e9cd38da90ea7190f96ca241d462d713290d93b
john <johnny>
2021-10-03 13:06:59 -0500

    bug fix: foreign key name fix for course and professor

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,19 +1,26 @@
-const db = require("../db")
-const Sequelize = require("sequelize")
+const db = require("../db");
+const Sequelize = require("sequelize");
 
-const Student = require("./student")
-const Professor = require("./professor")
-const Course = require("./course")
-const User = require("./user")
-
+const Student = require("./student");
+const Professor = require("./professor");
+const Course = require("./course");
+const User = require("./user");
 
 // 1 to many relationship
-Professor.hasMany(Course)
-Course.belongsTo(Professor)
+Professor.hasMany(Course, {
+  foreignKey: {
+    name: "professorId",
+  },
+});
+Course.belongsTo(Professor, {
+  foreignKey: {
+    name: "professorId",
+  },
+});
 
 module.exports = {
-    Student,
-    Professor,
-    Course,
-    User
-}
+  Student,
+  Professor,
+  Course,
+  User,
+};
