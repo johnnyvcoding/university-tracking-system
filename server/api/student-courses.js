aa8ff823f9d08fddf1c8ae193ce33198bbccfb94
john <johnny>
2021-10-04 17:45:00 -0500

    assign student to class route

--- server/api/student-courses.js
+++ server/api/student-courses.js
@@ -24,17 +24,39 @@ router.delete("/:studentCourseId", async (req, res, next) => {
       where: { id: studentCourseId },
     });
 
-
     // if student record  exists, then return a message
     // else return a message
     return studentCourseRecord
-      ? res.set({ "x-organization": "Skyline" }).json(studentCourseRecord).status(202)
+      ? res
+          .set({ "x-organization": "Skyline" })
+          .json(studentCourseRecord)
+          .status(202)
       : res
           .set({ "x-organization": "Skyline" })
           .json({ message: "Student record was not found" })
           .status(404);
   } catch (error) {
     console.log(error);
+    next(error);
+  }
+});
+
+// assigns a class (course) to a student
+router.post("/", async (req, res, next) => {
+  try {
+    let { courseId, studentId } = req.body;
+
+    let studentCourseRecord = await StudentCourse.create({
+      courseId: courseId,
+      studentId: studentId,
+    });
+
+    res
+      .set({ "x-organization": "Skyline" })
+      .json(studentCourseRecord)
+      .status(201);
+  } catch (error) {
+    console.log(errr);
     next(error);
   }
 });
