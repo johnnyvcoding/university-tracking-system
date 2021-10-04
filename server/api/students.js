99a268eaf047bb03802497368173f6b5264a1fe6
john <johnny>
2021-10-04 13:37:24 -0500

    delete and get single student routes

--- server/api/students.js
+++ server/api/students.js
@@ -3,7 +3,6 @@ const { Student } = require("../db/models");
 
 function studentQuerySearch() {}
 
-
 // get students based on their first and last name
 router.get("/", async (req, res, next) => {
   try {
@@ -12,14 +11,61 @@ router.get("/", async (req, res, next) => {
     let students = await Student.findAll({
       where: {
         firstName: firstName,
-        lastName:  lastName 
+        lastName: lastName,
       },
     });
 
-    return res
-      .set({ "x-organization": "Skyline" })
-      .json(students)
-      .status(200);
+    return res.set({ "x-organization": "Skyline" }).json(students).status(200);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// get a single student based on its id
+router.get("/:studentId", async (req, res, next) => {
+  try {
+    let { studentId } = req.params;
+
+    let student = await Student.findOne({
+      where: {
+        studentId: studentId,
+      },
+    });
+
+    // if student exists, then return a message
+    // else return a message
+    return student
+      ? res.set({ "x-organization": "Skyline" }).json(student).status(200)
+      : res
+          .set({ "x-organization": "Skyline" })
+          .json({ message: "Student was not found" }).status(404);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.delete("/:studentId", async (req, res, next) => {
+  try {
+    let { studentId } = req.params;
+
+    let student = await Student.findOne({
+      where: {
+        studentId: studentId,
+      },
+    });
+
+    // destroy record if a student exists
+    student ? await student.destroy() : null;
+
+    // if student exists, then return a message
+    // else return a message
+    return student
+      ? res.set({ "x-organization": "Skyline" }).json(student).status(202)
+      : res
+          .set({ "x-organization": "Skyline" })
+          .json({ message: "Student was not found" }).status(404);
   } catch (error) {
     console.log(error);
     next(error);
