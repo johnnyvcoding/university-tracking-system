aa532abefda7dd25ae124985ddd187e7ff20c9a9
john <johnny>
2021-10-04 17:30:21 -0500

    remove student from course

--- server/api/student-courses.js
+++ server/api/student-courses.js
@@ -1,6 +1,42 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const { StudentCourse, Student, Course } = require("../db/models");
+
+router.get("/", async (req, res, next) => {
+  try {
+    let studentCourses = await StudentCourse.findAll();
+
+    return res
+      .set({ "x-organization": "Skyline" })
+      .json(studentCourses)
+      .status(200);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// this route removes student from course
+router.delete("/:studentCourseId", async (req, res, next) => {
+  try {
+    let { studentCourseId } = req.params;
+
+    let studentCourseRecord = await StudentCourse.findOne({
+      where: { id: studentCourseId },
+    });
 
 
+    // if student record  exists, then return a message
+    // else return a message
+    return studentCourseRecord
+      ? res.set({ "x-organization": "Skyline" }).json(studentCourseRecord).status(202)
+      : res
+          .set({ "x-organization": "Skyline" })
+          .json({ message: "Student record was not found" })
+          .status(404);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
-
-module.exports = router
+module.exports = router;
