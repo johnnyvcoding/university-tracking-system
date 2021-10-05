b26b30d7488d9dd263d6cc1b76ea68aa01542ce4
john <johnny>
2021-10-04 20:31:52 -0500

    exam model

---
+++ server/db/models/exam.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Exam = db.define("Exam", {
+  examId: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  points: {
+    type: Sequelize.INTEGER,
+    default: 0,
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  assignedDate: {
+    type: Sequelize.DATEONLY,
+    validate: {
+      isDate: true,
+    },
+  },
+  deadlineDate: {
+    type: Sequelize.DATEONLY,
+    validate: {
+      isDate: true,
+    },
+  },
+});
+
+module.exports = Course;
