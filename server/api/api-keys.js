b0a8b3e15ce6cc7ce5e3c93ea99f1c728d29b79b
john <johnny>
2021-10-04 19:46:32 -0500

    post api key for user

--- server/api/api-keys.js
+++ server/api/api-keys.js
@@ -7,22 +7,40 @@ const crypto = require("crypto");
 // const vectorKey = crypto.randomBytes(16)
 // const decipher = crypto.createDecipheriv(algorithm, securityKey, vectorKey)
 
-router.get("/", async (req, res, next) => {
+function generateKey() {
+  //creates a base 36 string character
+  // letters can range from a-z
+  // nummbers 0-9
+  return [...Array(30)]
+    .map((e) => ((Math.random() * 36) | 0).toString(36))
+    .join("");
+}
+
+// in order for these routes to be accessed,
+// a user must be originally seeded manually
+router.post("/", async (req, res, next) => {
   try {
-    let { apiKey } = req.query;
+    let { userId } = req.body;
 
-    let userOne = await Apikey.findOne({
-        where: {
-            UserId: 1
-        }
-    })
-
-    if (userOne.correctKey(apiKey)) {
-        return res.json({message: "Correct"})
+    if (!userId) {
+      return res
+        .set({ "x-organization": "Skyline" })
+        .json({ message: "No user was selected" })
+        .status(400);
     }
 
+    let generatedKey = generateKey();
 
-    return res.json(userOne);
+    // passing the user id will assign a new key
+    let key = await Apikey.create({
+      key: generatedKey,
+      UserId: userId,
+    });
+
+    return res
+      .set({ "x-organization": "Skyline", "Content-Type": "application/json" })
+      .json({ api_key: generatedKey })
+      .status(201);
   } catch (error) {
     console.log(error);
     next(error);
