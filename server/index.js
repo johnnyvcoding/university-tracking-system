9fc3b24b30f8f72d0151a5ed19d537a445737781
Johnny Vazquez <johnnyvcoding@gmail.com>
2021-10-07 00:44:33 -0500

    add testing hooks and export server

--- server/index.js
+++ server/index.js
@@ -18,6 +18,15 @@ const {verifyKey} = require("./middleware")
 
 const PORT = process.env.PORT || 8080;
 
+module.exports = app
+
+// This is a global Mocha hook, used for resource cleanup.
+// Otherwise, Mocha v4+ never quits after tests.
+if (process.env.NODE_ENV === 'test') {
+  after('close the session store', () => sessionStore.stopExpiringSessions())
+}
+
+
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id));
 
@@ -78,8 +87,11 @@ async function bootApp() {
   await startListening();
 }
 
+
+
 if (require.main === module) {
   bootApp();
 } else {
   createApp();
 }
+
